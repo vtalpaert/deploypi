2a29aa469c73dfbbe156bfed1c8264b2a9c299f9
Victor Talpaert <victor.talpaert@akka.eu>
2018-10-01 23:37:31 +0200

    now with sudo -i for working piping

--- install_ap.sh
+++ install_ap.sh
@@ -9,24 +9,35 @@ echo "Type the password for your home WiFi"
 read home_password
 echo "Your Wifi has ssid $home_ssid and password $home_password"
 
+echo "What ssid would you like for the access point?"
+read ap_ssid
 echo "Type the password for the AP"
 read ap_password
-echo "Your password is $ap_password"
+echo "Your AP will have ssid $ap_ssid and password $ap_password"
 
-wlan0_mac=`ifconfig wlan0 | awk '/^[a-z]/ { iface=$1; mac=$NF; next } /inet addr:/ { print mac }'`
+wlan0_mac=`cat /sys/class/net/wlan0/address`
 echo "Your MAC address for WiFi is $wlan0_mac"
 
-sudo rpi-update
+cp config/start-ap-managed-wifi.sh ~/
+chmod u+x ~/start-ap-managed-wifi.sh
+
+crontab -l | cat - config/crontab-fragment.txt > crontab.txt
+crontab crontab.txt
+rm crontab.txt
+
+sudo -i
+
+rpi-update
 
 
 ## install wlan + ap
 
-sudo cp config/70-persistent-net.rules /etc/udev/rules.d/
-sudo sed -i -e "s/wlan0_mac/$wlan0_mac/g" /etc/udev/rules.d/70-persistent-net.rules
+cp config/70-persistent-net.rules /etc/udev/rules.d/
+sed -i -e "s/wlan0_mac/$wlan0_mac/g" /etc/udev/rules.d/70-persistent-net.rules
 
-sudo apt-get install dnsmasq hostapd
+apt-get install dnsmasq hostapd
 
-sudo echo "
+echo "
 # https://albeec13.github.io/2017/09/26/raspberry-pi-zero-w-simultaneous-ap-and-managed-mode-wifi/
 interface=lo,ap0
 no-dhcp-interface=lo,wlan0
@@ -36,24 +47,19 @@ domain-needed
 bogus-priv
 dhcp-range=192.168.10.50,192.168.10.150,12h" >> /etc/dnsmasq.conf
 
-sudo cp config/hostapd.conf /etc/hostapd/
-sudo sed -i -e "s/ap_password/$ap_password/g" /etc/hostapd/hostapd.conf
+cp config/hostapd.conf /etc/hostapd/
+sed -i -e "s/ap_ssid/$ap_ssid/g" /etc/hostapd/hostapd.conf
+sed -i -e "s/ap_password/$ap_password/g" /etc/hostapd/hostapd.conf
 
-sudo echo '
+echo '
 DAEMON_CONF="/etc/hostapd/hostapd.conf"' >> /etc/default/hostapd
 
-sudo cp config/wpa_supplicant.conf /etc/wpa_supplicant/
-sudo sed -i -e "s/ap_password/$ap_password/g" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e "s/home_ssid/$home_ssid/g" /etc/wpa_supplicant/wpa_supplicant.conf
-sudo sed -i -e "s/home_password/$home_password/g" /etc/wpa_supplicant/wpa_supplicant.conf
+cp config/wpa_supplicant.conf /etc/wpa_supplicant/
+sed -i -e "s/ap_ssid/$ap_ssid/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sed -i -e "s/ap_password/$ap_password/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sed -i -e "s/home_ssid/$home_ssid/g" /etc/wpa_supplicant/wpa_supplicant.conf
+sed -i -e "s/home_password/$home_password/g" /etc/wpa_supplicant/wpa_supplicant.conf
 
-sudo cp config/interfaces /etc/network/
+cp config/interfaces /etc/network/
 
-sudo update-rc.d dhcpcd disable
-
-cp config/start-ap-managed-wifi.sh ~/
-chmod u+x ~/start-ap-managed-wifi.sh
-
-crontab -l | cat - config/crontab-fragment.txt > crontab.txt
-crontab crontab.txt
-rm crontab.txt
+update-rc.d dhcpcd disable
